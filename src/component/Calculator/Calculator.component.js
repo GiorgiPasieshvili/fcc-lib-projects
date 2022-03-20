6d4701d70f19381d9bc26c10f096a25135177368
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-03-21 02:42:03 +0400

    Build Calculator

--- src/component/Calculator/Calculator.component.js
+++ src/component/Calculator/Calculator.component.js
@@ -1,56 +1,56 @@
 import { PureComponent } from 'react';
+import CalculatorCharacters from './CalculatorCharacters';
+import './Calculator.style.scss';
 
 /** @namespace Component/App/Component */
 export class Calculator extends PureComponent {
 
-    onButtonClick() {
-        return true
+    renderCharacters() {
+        const { onButtonClick } = this.props;
+
+        const calculatorButtons = CalculatorCharacters.map(character => (
+                <button
+                    id={ character.name }
+                    key={ character.name }
+                    onClick={ () => onButtonClick(character) }
+                >
+                    { character.value }
+                </button>
+            )
+        );
+
+        return (
+            <div className='Calculator-Buttons'>
+                { calculatorButtons }
+            </div>
+        );
+    }
+
+    renderOutput() {
+        const { currentOperation, wholeOperation } = this.props;
+
+        return (
+            <div className='Calculator-Output'>
+                {/* Render Whole Operation */}
+                <div className='Calculator-Output_Whole'>
+                    { wholeOperation }
+                </div>
+
+                {/* Render Current Chosen Operator or Number */}
+                <div className='Calculator-Output_Current'>
+                    { currentOperation }
+                </div>
+            </div>
+        );
     }
 
     render() {
         return (
-            <div>
-                <h1>Calculator</h1>
-
-                {/* Calculator Container */}
-                <div className='Calculator'>
-
-                    {/* Render Screen */}
-                    <div className='Calculator-Screen'>
-                        {/* Render Whole Operation */}
-                        <div className='Calculator-Screen_Top'></div>
-
-                        {/* Render Current Chosen Operator or Number */}
-                        <div className='Calculator-Screen_Bottom'>0</div>
-                    </div>
-
-                    <div className='Calculator-Buttons'>
-                        {/* Render Number Buttons */}
-                        <button id="zero" onclick={this.onButtonClick}>0</button>
-                        <button id="one" onclick={this.onButtonClick}>1</button>
-                        <button id="two" onclick={this.onButtonClick}>2</button>
-                        <button id="three" onclick={this.onButtonClick}>3</button>
-                        <button id="four" onclick={this.onButtonClick}>4</button>
-                        <button id="five" onclick={this.onButtonClick}>5</button>
-                        <button id="six" onclick={this.onButtonClick}>6</button>
-                        <button id="seven" onclick={this.onButtonClick}>7</button>
-                        <button id="eight" onclick={this.onButtonClick}>8</button>
-                        <button id="nine" onclick={this.onButtonClick}>9</button>
-
-                        {/* Render Operator Buttons*/}
-                        <button id="add" onclick={this.onButtonClick}>+</button>
-                        <button id="subtract" onclick={this.onButtonClick}>-</button>
-                        <button id="multiply" onclick={this.onButtonClick}>*</button>
-                        <button id="divide" onclick={this.onButtonClick}>/</button>
-
-                        {/* Render Other Buttons */}
-                        <button id="equals" onclick={this.onButtonClick}>=</button>
-                        <button id="decimal" onclick={this.onButtonClick}>.</button>
-                        <button id="clear" onclick={this.onButtonClick}>AC</button>
-                    </div>
-                </div>
+            <div className='Calculator'>
+                { this.renderOutput() }
+                { this.renderCharacters() }
             </div>
-        )
+        );
     }
 }
 
--- src/component/Calculator/Calculator.container.js
+++ src/component/Calculator/Calculator.container.js
@@ -4,8 +4,124 @@ import Calculator from './Calculator.component';
 /** @namespace Component/App/Container */
 export class CalculatorContainer extends PureComponent {
 
+    state = {
+        currentOperation: '0',
+        wholeOperation: '',
+        operators: /([*/+-])/g
+    };
+
+    containerFunctions = {
+        onButtonClick: this.onButtonClick.bind(this)
+    };
+
+    containerProps() {
+        const { currentOperation, wholeOperation } = this.state;
+
+        return {
+            currentOperation,
+            wholeOperation
+        };
+    };
+
+    onNumberButtonClick(value) {     
+      const { currentOperation, wholeOperation, operators } = this.state;
+      let currentInstance = currentOperation;
+      let wholeInstance = wholeOperation;
+
+      if(currentInstance.match(operators) || currentInstance === '0') {
+        currentInstance = '';
+      }
+
+      if(wholeInstance.match('=')) {
+        currentInstance = '';
+        wholeInstance = '';
+      }
+
+      this.setState({
+        currentOperation: currentInstance + value,
+        wholeOperation: wholeInstance + value
+      });
+    }
+
+    onOperatorButtonClick(value) {
+      const { currentOperation, wholeOperation, operators } = this.state;
+      let currentInstance = currentOperation;
+      let wholeInstance = wholeOperation;
+
+      if(currentInstance.match(operators)) {
+        wholeInstance = wholeInstance.slice(0, wholeInstance.length - 1);
+      }
+
+      if(wholeInstance.match('=')) {
+        wholeInstance = currentInstance;
+      }
+
+      this.setState({
+        currentOperation: value,
+        wholeOperation: wholeInstance + value
+      });
+    }
+
+    onClearButtonClick() {
+      this.setState({
+        currentOperation: '0',
+        wholeOperation: ''
+      })
+    }
+    
+    onPointButtonClick() {
+      const { currentOperation, wholeOperation } = this.state;
+      let currentInstance = currentOperation;
+      let wholeInstance = wholeOperation;
+      
+      if(currentInstance.indexOf(".") < 0){
+        this.setState({
+          currentOperation: currentInstance + '.',
+          wholeOperation: wholeInstance + '.'
+        });
+      }
+    }
+
+    onMatchButtonClick() {
+      const { currentOperation, wholeOperation, operators } = this.state;
+      let currentInstance = currentOperation;
+      let wholeInstance = wholeOperation;
+
+      if(currentInstance.match(operators)){
+        wholeInstance = wholeInstance.slice(0, wholeInstance.length - 1);
+      }
+
+      this.setState({
+        currentOperation: JSON.stringify(eval(wholeInstance)),
+        wholeOperation: wholeInstance + '=' + eval(wholeInstance)
+      });
+    }
+
+    onButtonClick(button) {
+        const { type, value } = button;
+
+        if(type === 'number') {
+          this.onNumberButtonClick(value);
+        } else if (type === 'operator') {
+          this.onOperatorButtonClick(value);
+        } else if (type === 'clear') {
+          this.onClearButtonClick();
+        } else if (type === 'point') {
+          this.onPointButtonClick();
+        } else if (type === 'match') {
+          this.onMatchButtonClick();
+        } else {
+          console.log('This button type is not exist')
+        }
+    }
+
     render() {
-        return <Calculator />
+        return (
+            <Calculator 
+                { ...this.containerFunctions }
+                { ...this.containerProps() }
+            />
+        );
     }
 }
 
